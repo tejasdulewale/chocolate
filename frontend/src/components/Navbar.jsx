e81abd47ba60961f3a214c06485ae5a479fcce24
tejasdulewale <dulewaletejas11@gmail.com>
2025-06-12 11:58:29 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,15 +7,49 @@ const Navbar = () => {
     <div className="navbar">
       <div className="logo">chocolate's</div>
       <nav>
-        <Link to="/" onClick={()=>{setUnderline("Home")}} className={underline==="Home"?"active":""}>Home</Link>
-        <Link to="/about" onClick={()=>{setUnderline("About")}} className={underline==="About"?"active":""}>About</Link>
-        <Link to="/contact" onClick={()=>{setUnderline("Contact")}} className={underline==="Contact"?"active":""}>Contact</Link>
-        <Link to="/product" onClick={()=>{setUnderline("Product")}} className={underline==="Product"?"active":""}>Product</Link>
+        <Link
+          to="/"
+          onClick={() => {
+            setUnderline("Home");
+          }}
+          className={underline === "Home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          onClick={() => {
+            setUnderline("About");
+          }}
+          className={underline === "About" ? "active" : ""}
+        >
+          About
+        </Link>
+        <Link
+          to="/contact"
+          onClick={() => {
+            setUnderline("Contact");
+          }}
+          className={underline === "Contact" ? "active" : ""}
+        >
+          Contact
+        </Link>
+        <Link
+          to="/product"
+          onClick={() => {
+            setUnderline("Product");
+          }}
+          className={underline === "Product" ? "active" : ""}
+        >
+          Product
+        </Link>
       </nav>
 
       <div className="icons">
         <i class="fa-solid fa-cart-shopping"></i>
-        <i class="fa-solid fa-user"></i>
+        <Link to="/register">
+          <i class="fa-solid fa-user"></i>
+        </Link>
       </div>
     </div>
   );
